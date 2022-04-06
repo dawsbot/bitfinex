bc7f70ca4a1965296e806803398cb15818b36977
Dawson Botsford <github.81841d@tryninja.io>
2022-04-06 11:19:21 -0700

    Remove author email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='FinexAPI',
       version='1.0',
       description='BitFinex API wrapper',
       author='jndok & Dawson Botsford',
-      author_email='dawsonbotsford@gmail.com',
+      author_email='',
       url='https://github.com/dawsonbotsford/bitfinex',
       packages=['FinexAPI'],
      )
